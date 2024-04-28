02ea15822a80b6d6e9bc24ecab77c68a5e54ccc5
GuKBABjOa <128679235+GuKBABjOa@users.noreply.github.com>
2024-04-28 21:56:23 +0900

    9461

---
+++ DP/9461.py
@@ -0,0 +1,10 @@
+def solve(n):
+    dp = [0, 1, 1, 1, 2, 2] + [0] * (n - 5)
+    for i in range(6, n + 1):
+        dp[i] = dp[i - 1] + dp[i - 5]
+    return dp[n]
+
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    print(solve(n))
