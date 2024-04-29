ec8c2466b70cfe5088357922423f017a1e9d9df7
GuKBABjOa <128679235+GuKBABjOa@users.noreply.github.com>
2024-04-29 17:43:42 +0900

    9461

--- DP/9461.py
+++ DP/9461.py
@@ -1,4 +1,4 @@
-def solve(n):
+def wave(n):
     dp = [0, 1, 1, 1, 2, 2] + [0] * (n - 5)
     for i in range(6, n + 1):
         dp[i] = dp[i - 1] + dp[i - 5]
@@ -7,4 +7,4 @@ def solve(n):
 t = int(input())
 for _ in range(t):
     n = int(input())
-    print(solve(n))
+    print(wave(n))
