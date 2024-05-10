132eb5334885dd51b8371835667dcc9a37ad5939
GuKBABjOa <128679235+GuKBABjOa@users.noreply.github.com>
2024-05-10 21:46:42 +0900

    10451

---
+++ Gragh/10451.py
@@ -0,0 +1,30 @@
+import sys
+import collections
+
+sys.setrecursionlimit(10**6)
+
+def graph(n, p):
+    g = collections.defaultdict(int)
+    for i in range(1, n+1):
+        g[i] = p[i-1]
+    return g
+
+def dfs(u):
+    global g
+    global visited
+    visited[u] = 1
+    if not visited[g[u]]:
+        dfs(g[u])
+
+t = int(sys.stdin.readline().strip())
+for _ in range(t):
+    n = int(sys.stdin.readline().strip())
+    p = [int(x) for x in sys.stdin.readline().split()]
+    cnt = 0
+    g = graph(n, p)
+    visited = [0 for _ in range(n+1)]
+    for i in range(1, n+1):
+        if not visited[i]:
+            dfs(i)
+            cnt += 1
+    print(cnt)
